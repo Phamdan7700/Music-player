139c91f6b0570a28b291fa5782144df8b8eab355
phamdan <phamdan7700@gmail.com>
2021-10-13 16:24:24 +0700

    add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const songs = [
+const playListData = [
     {
         id: 1,
         title: "Người Lạ Thoáng Qua",
@@ -11,21 +11,21 @@ const songs = [
         title: "Em Là Con Thuyền Cô Đơn",
         artist: "Thái Học",
         thumbnail: 'Dinhtunghuy.jpg',
-        audio: 'CauHenCauThe-DinhDung-6994741.mp3'
+        audio: 'EmLaConThuyenCoDonACVRemix-ThaiHoc-7098730.mp3'
     },
     {
         id: 3,
         title: "Sai Cách Yêu",
         artist: "Lê Bảo Bình",
         thumbnail: 'lebaobinh.jpg',
-        audio: 'CauHenCauThe-DinhDung-6994741.mp3'
+        audio: 'SaiCachYeuRemixVersion2-LeBaoBinh-7064489.mp3'
     },
     {
         id: 4,
-        title: "Người Lạ Thoáng Qua",
-        artist: "Đinh Tùng Huy, ACV",
-        thumbnail: 'Dinhtunghuy.jpg',
-        audio: 'CauHenCauThe-DinhDung-6994741.mp3'
+        title: "Cô Đơn Dành Cho Ai",
+        artist: "Lee Ken, Nal",
+        thumbnail: 'codondanhchoai.jpg',
+        audio: 'CoDonDanhChoAi-LeeKenNal-7068986.mp3'
     },
     {
         id: 5,
@@ -73,20 +73,123 @@ const songs = [
 
 class MusicPlayer {
     constructor() {
-        this.song = document.getElementById('song')
+        this.playListData = playListData;
+        this.isPlaying = false
+        this.currentSong = {
+            audio: null,
+            index: null
+        };
+        this.duration = 0;
+
         this.ui = new UI();
-        this.play(this.song);
-        this.ui.renderPlayList(songs)
-        
+        this.ui.renderPlayList(this.playListData)
+        this.start();
     }
 
-    play(song) {
-        window.onclick = function() {
-            // console.log(song);
-            song.play();
+    start() {
+
+        let self = this;
+        let playList = Array.from(document.getElementsByClassName('song'));
+        playList.forEach((song, index) => {
+            song.addEventListener('click', function () {
+                self.loadCurrentSong(index);
+            })
+        });
+
+        this.ui.playBtn.onclick = function () {
+            self.isPlaying ? self.pause() : self.play();
+        }
+
+        this.ui.forwardBtn.onclick = function () {
+            self.next(1)
+        }
+
+        this.ui.backwardBtn.onclick = function () {
+            self.next(-1)
+        }
+
+        this.ui.randomBtn.onclick = function () {
+            self.random();
+        }
+
+
+
+
+
+    }
+
+    play() {
+        if (this.currentSong.audio) {
+            this.isPlaying = true;
+            this.currentSong.audio.play();
+            this.ui.playBtn.innerHTML = '<i class="far fa-pause-circle"></i>'
+            this.ui.thumbnail.style.animationPlayState = 'running';
 
         }
+
     }
+
+    pause() {
+        if (this.currentSong.audio) {
+            this.isPlaying = false;
+            this.currentSong.audio.pause();
+            this.ui.playBtn.innerHTML = '<i class="far fa-play-circle"></i>'
+            this.ui.thumbnail.style.animationPlayState = 'paused';
+        }
+    }
+
+    next(n) {
+        this.currentSong.index += n
+        let lastIndexAudio = this.playListData.length - 1
+        if (this.currentSong.index < 0) {
+            this.currentSong.index = 0
+        }
+
+        if (this.currentSong.index > lastIndexAudio) {
+            this.currentSong.index = lastIndexAudio
+        }
+
+        this.loadCurrentSong(this.currentSong.index)
+
+    }
+
+    random() {
+        this.currentSong.index = Math.floor(Math.random() * this.playListData.length)
+        this.loadCurrentSong(this.currentSong.index)
+    }
+
+
+    loadCurrentSong(currentSongIndex) {
+        this.ui.thumbnail.src = 'assets/img/' + playListData[currentSongIndex].thumbnail;
+        this.pause();
+        this.currentSong.audio = new Audio('assets/music/' + playListData[currentSongIndex]['audio']);
+        this.currentSong.index = currentSongIndex;
+        this.play();
+        this.ui.activeCurrentSong(this.ui.playList[currentSongIndex]);
+
+        this.currentSong.audio.ontimeupdate = () => {
+            this.duration = this.currentSong.audio.duration
+            this.ui.durationControl.max = this.duration
+            this.ui.durationControl.value = this.currentSong.audio.currentTime;
+            
+        }
+
+        this.ui.durationControl.oninput = (e) => {
+            this.currentSong.audio.currentTime = e.target.value
+        }
+
+        this.currentSong.audio.onended = () => {
+            this.loadCurrentSong(this.currentSong.index)
+        }
+
+    }
+
+    editTime() {
+
+    }
+
+
+
 
 
 }
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,24 +1,24 @@
 
 
 class UI {
-    constructor(songs) {
-        this.playBtn      = document.getElementById('btn-play');
-        this.repeatBtn    = document.getElementById('btn-repeat');
-        this.backwardBtn  = document.getElementById('btn-backward');
-        this.forwardBtn   = document.getElementById('btn-forward');
-        this.randomBtn    = document.getElementById('btn-random');
-        this.playList    = document.getElementById('play-list');
+    constructor() {
+        this.playBtn = document.getElementById('btn-play');
+        this.repeatBtn = document.getElementById('btn-repeat');
+        this.backwardBtn = document.getElementById('btn-backward');
+        this.forwardBtn = document.getElementById('btn-forward');
+        this.randomBtn = document.getElementById('btn-random');
+        this.playList = document.getElementsByClassName('song');
+        this.thumbnail = document.querySelector('#thumbnail img')
+        this.durationControl = document.getElementById('duration')
 
-        this.songs = songs;
-        this.isPlaying = false
 
     }
 
 
-    renderPlayList(listData) {
-        let listDataArr = listData.map((song => {
+    renderPlayList(playListData) {
+        let listDataArr = playListData.map((song) => {
             return `
-            <li class="song">
+            <li class="song" data-sound="${song.audio}">
                 <img src="./assets/img/${song.thumbnail}" alt="">
                 <div class="song-info">
                     <h3>${song.title}</h3>
@@ -27,30 +27,20 @@ class UI {
                 <span><i class="fas fa-ellipsis-h"></i></span>
             </li>
             `;
-        }));
-
-        this.playList.innerHTML = listDataArr.join('');
-
-        let songs = this.playList.querySelectorAll('.song');
-        songs.forEach(song => {
-            song.addEventListener('click', function() {
-                document.querySelector('.song.active')?.classList.remove('active')
-                this.classList.add('active')
-            })
-        })
+        });
+        let playListWraper = document.getElementById('playlist-wrapper');
+        playListWraper.innerHTML = listDataArr.join('');
     }
 
-    handlePlay(currentSong) {
 
-        this.playBtn.onclick = function(){
-            if (this.isPlaying) {
-                this.innerHTML  = '<i class="far fa-play-circle"></i>';
-            }else {
-                this.innerHTML = '<i class="far fa-pause-circle"></i>'
-            }
 
-            this.isPlaying = !this.isPlaying;
-            
-        }
+    activeCurrentSong(currentSong) {
+        let activeSong = document.querySelector('.song.active')
+        activeSong?.classList.remove('active');
+        currentSong.classList.add('active')
+        this.thumbnail.classList.add('rotate');
     }
+
+
+
 }
